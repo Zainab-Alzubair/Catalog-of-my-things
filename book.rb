c6c2ca9cf5c48cb0d12dd260c7fbba9a507d0089
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-02-28 20:59:15 +0200

    add Book class

---
+++ book.rb
@@ -0,0 +1,14 @@
+require_relative 'classes/title'
+
+class book < Item
+  attr_accessor :title, :author, :pages, :cover_state
+
+  def initialize(title, author, pages, cover_state)
+    @title = title
+    @author = author
+    @pages = pages
+    @cover_state = cover_state
+  end 
+end
+
+    
