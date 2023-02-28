a67e59d508ea18e6c7dabe1fea8a73333e440eb1
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-02-28 21:38:20 +0200

    created json files in Book and Label classes

--- book.rb
+++ book.rb
@@ -17,6 +17,32 @@ class book < Item
       return super
     end
   end
+
+  def save
+    File.open('books.json', 'a') do |file|
+        file.puts JSON.generate(self.to_h)
+    end
+  end
+
+  def to_h
+    {
+        title: @title,
+        author: @author,
+        pages: @pages,
+        cover_state: @cover_state
+    }
+  end
+
+  def self.all
+    books = []
+    File.open('books.json', 'r') do |file|
+        file.each_line do |line|
+            book_data = JSON.parse(line, symbolize_names: true)
+            books << self.new(book_data[:title], book_data[:author], book_data[:pages], book_data[:cover_state])
+        end
+    end
+    books
+  end
 end
 
     
