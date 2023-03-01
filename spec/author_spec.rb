b8f36a7320f2b7411cf4ed5b12c0c4fd356d2704
odaialazzeh <odai.alazzeh@gmail.com>
2023-03-01 16:14:46 +0200

    Create test for author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../classes/author'
+require_relative '../classes/title'
+
+describe Author do
+  author = Author.new('Rames', 'Jaynor')
+  describe '#initialize' do
+    it 'should create a new Author' do
+      expect(author).to be_a Author
+    end
+
+    it 'the new Author\'s first name should be Rames' do
+      expect(author.first_name).to eq('Rames')
+    end
+
+    it 'the new Author\'s last name should be Jaynor' do
+      expect(author.last_name).to eq('Jaynor')
+    end
+  end
+
+  describe '#add_item' do
+    it 'should add an item to the items array' do
+      item = Item.new(Time.new(2000, 1, 1), true, 10)
+
+      author.add_item(item)
+
+      expect(author.items.length).to eq(1)
+    end
+  end
+end
