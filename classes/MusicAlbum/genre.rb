c8a7c9c894fb87d5cbfe902aaf24a2ec02e4ff74
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-03-02 02:27:25 +0300

    Add MusicAlbum and Genre classes

---
+++ classes/MusicAlbum/genre.rb
@@ -0,0 +1,13 @@
+class Genre
+  attr_accessor :id, :name, :items
+  def initialize(id,name,items)
+    @id = id
+    @name = name
+    @items = []
+  end
+
+  def add_item(item) 
+    @items << item
+    item.genre = self if item.genre.nil?
+  end
+end
