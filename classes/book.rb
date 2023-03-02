d67ad6edee675d29586b1fae8c031208bc2ca7ae
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-03-02 10:58:52 +0200

    fixed linter errors

---
+++ classes/book.rb
@@ -0,0 +1,19 @@
+require_relative 'classes/title'
+
+class Book < Item
+  attr_accessor :title, :author, :publish_date, :cover_state
+
+  def initialize(title, author, publish_date, cover_state)
+    super(publish_date)
+    @title = title
+    @author = author
+    @publish_date = publish_date
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    return true if @cover_state == 'bad'
+
+    super
+  end
+end
---
+++ classes/label.rb
@@ -0,0 +1,14 @@
+require 'json'
+class Label
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
